3220ab239beec91702b670a4e65ac708d2d22dbd
Pierre-vh <pierre.vanhoutryve@outlook.com>
2019-07-15 13:11:22 +0200

    Centering the map now uses nominatim queries instead of hardcoded presets

--- SlippyMaps.js
+++ SlippyMaps.js
@@ -1,25 +1,27 @@
 /**
- * The unique identifiers given to cities for the
- * Configuration.centerOnCity function
+ * The unique identifiers given to some basic, built-in queries.
  */
-const City = {
-  Gent: 1,
-  Brugge: 2,
-  Kortrijk: 3,
-  Antwerpen: 4,
-  Brussel: 5,
-  Bruxelles: 5,
+const BasicQuery = {
+  // Show factories
+  Factories: 1,
+  // Show chimneys
+  Chimneys: 2,
+  // Show breweries
+  Breweries: 3,
 }
 
 /**
- * The Configuration object used to set the properties of the map.
+ * The Configuration of a map.
+ * 
+ * For the configuration to be applied, you must pass it to showMap or call updateMap manually.
+ * The configuration is completely separate from the map instance.
  */
 class Configuration {
   /**
    * Set to true if we should override the configuration based on attributes
    * in the map's div.
    */
-  canOverrideFromDivAttributes = true;
+  canUseDivAttributes = true;
   /**
    * The Center of the Map.
    * Default Parameter is roughly in the center of Belgium.
@@ -27,9 +29,9 @@ class Configuration {
   viewCenter = [50.605902641613284, 4.752960205078125];
   /**
    * The Zoom level of the map
-   * Default parameter is 9, enough to see nearly every part of Belgium.
+   * Default parameter is 14.
    */
-  viewZoom = 9;
+  viewZoom = 14;
   /**
    * The TileLayer to use.
    * See https://leafletjs.com/reference-1.5.0.html#tilelayer for more information.
@@ -50,10 +52,10 @@ class Configuration {
   /**
    * Override this configuration object's member from a div's attribute.
    * The attributes must be named the same as the members, and they are checked
-   * to see if the attribute's String is well-formed.
+   * to see if the attribute's string is well-formed.
    * @param {*} div the div object
    */
-  overrideFromDivAttributes(div) {
+  useDivAttributes(div) {
     // Utils
     function warnIllFormedAttr(attrName, value) {
       console.warn('Ill-formed attribute for %o: %o', attrName, value);
@@ -63,7 +65,7 @@ class Configuration {
     let hasSetViewCenter = false;
     if (div.hasAttribute('viewCenter')) {
       let value = div.getAttribute('viewCenter');
-      // Must be a String with 2 floating-point numbers, separated by a comma
+      // Must be a string with 2 floating-point numbers, separated by a comma
       // and between brackets. e.g [3.14, 3.14]
       if (/^\[(\d+.\d+)\s?,\s?(\d+.\d+)\]$/.test(value)) {
         this.viewCenter = JSON.parse(value);
@@ -100,110 +102,54 @@ class Configuration {
       else
         warnIllFormedAttr('maxElements', value);
     }
-
-    // Also accept a 'city' attribute (which should be a value in the 'City' constant)
-    // to focus on a given city.
-    // If the user has both a viewCenter and city attribute, warn it.
-    if(div.hasAttribute('city')) {
-      let city = City[div.getAttribute('city')];
-      if(city == undefined)
-        warnIllFormedAttr('city', 'unknown city (undefined)');
-      else {
-        this.focusOnCity(city);
-        if(hasSetViewCenter)
-          console.warn("viewCenter attribute was overriden by city attribute");
-      }
-    }
-  }
-
-  /**
-   * Centers the map on a given city
-   * @param {number} city The unique identifier of a City, @see City
-   * @param {Boolean} shouldZoom If set to true, the zoom level will be adjusted as well to
-   *                          make the whole city visible. Default is true.
-   */
-  focusOnCity(city, shouldZoom = true) {
-    /**
-     * Helper setter.
-     * @param {Configuration} instance current instance of the Configuration class (this). 
-     *                        It has to be passed as parameter because 'this' isn't available 
-     *                        inside the body of this function. (Or more precisely, it's not
-     *                        the 'this' we're looking for)
-     * @param {number} lat latitude of the view center
-     * @param {number} lng longitude of the view center
-     * @param {number} zoom zoom of the view (will only be set if shouldZoom === true)
-     */
-    function set(instance, lat, lng, zoom) {
-      instance.viewCenter = [lat, lng];
-      if(shouldZoom === true) 
-      instance.viewZoom = zoom;
-    }
-    // Switch to the correct city.
-    switch(city) {
-      default:
-        console.error("Can't center on city - unknown city");
-      case undefined:
-        console.error("Can't center on city - no city given (undefined)");
-        break;
-      case City.Gent:
-        set(this, 51.05364274168696, 3.7278842926025395, 14);
-        break;
-      case City.Brugge:
-        set(this, 51.20962577701306, 3.2314395904541016, 14);
-        break;
-      case City.Kortrijk:
-        set(this, 50.829360798981426, 3.2680034637451176, 14);
-        break;
-      case City.Antwerpen:
-        set(this, 51.2187121115024, 4.407062530517579, 13);
-        break;
-      case City.Brussel:
-        set(this, 50.84692264194612, 4.355821609497071, 14);
-        break;
-    }
   }
 }
 
 /**
  * Creates and displays a map in place of the div with id mapId
- * @param {String} mapId The HTML id of the div that should become the map
- * @param {Configuration} configuration the Configuration object. 
+ * @param {string} mapId The HTML id of the div that should become the map
+ * @param {Configuration} conf the Configuration object. 
  *                        Can be undefined. If that's the case, a default-constructed object will be used.
  *                        However, if you want to reuse the configuration object later, it's better to pass one.
  *  
- * If configuration.canOverrideFromDivAttributes is set to true, this will call 
- * configuration.overrideFromDivAttributes to update the configuration based on the attributes
+ * If conf.canUseDivAttributes is set to true, this will call 
+ * conf.useDivAttributes to update the configuration based on the attributes
  * of the div.
  */
-function showMap(mapId, configuration) {
-  if (configuration == undefined)
-    configuration = new Configuration();
+function showMap(mapId, conf) {
+  if (conf == undefined)
+    conf = new Configuration();
 
-  let div = document.getElementById('mapid');
+  let div = document.getElementById(mapId);
   if (div == null) {
     console.error("Cannot create map - div with id '%o' does not exist", mapId);
     return;
   }
 
-  if (configuration.canOverrideFromDivAttributes) {
-    configuration.overrideFromDivAttributes(div);
+  if (conf.canUseDivAttributes) {
+    conf.useDivAttributes(div);
   }
 
   let map = L.map('mapid');
-  updateMap(map, configuration);
+  updateMap(map, conf);
+
+  // If the div has a "focusOn" attribute, call focusOn on the div.
+  if(div.hasAttribute('focusOn'))
+    focusOn(map, div.getAttribute('focusOn'));
+
   return map;
 }
 
-
 /**
  * Updates a map using a configuration object.
- * This will remove all map layers as well if "removeOldLayers" is set to true.
+ * 
  * @param {Leaflet.Map} map the map
- * @param {Configuration} configuration the configuration object
- * @param {Boolean} removeOldLayers if set to true, old layers will be removed. Default is true.
+ * @param {Configuration} conf the configuration object
+ * @param {boolean} removeOldLayers if set to true, the old layers of the map 
+ *                                  will be removed. Default is true.
  */
-function updateMap(map, configuration, removeOldLayers = true) {
-  if(configuration === undefined) {
+function updateMap(map, conf, removeOldLayers = true) {
+  if(conf === undefined) {
     console.error("updateMap - no configuration given");
     return;
   }
@@ -211,20 +157,80 @@ function updateMap(map, configuration, removeOldLayers = true) {
     console.error("updateMap - no map given");
     return;
   }
-  map.setView(configuration.viewCenter, configuration.viewZoom);
+  map.setView(conf.viewCenter, conf.viewZoom);
   if(removeOldLayers === true)
     map.layers = [];
-  L.tileLayer(configuration.tileLayer, { attribution: configuration.tileLayerAttribution }).addTo(map);
+  L.tileLayer(conf.tileLayer, { attribution: conf.tileLayerAttribution }).addTo(map);
+}
+
+
+  /**
+   * Centers the map on something.
+   * This will perform a search using Nominatim, and center the map on the first result.
+   * If no result is found, the coordinates are unchanged.
+   * 
+   * The nominatim query will be performed using the following options:
+   *    - limit=1 
+   *    - viewbox=current bounds (map.getBounds().toBBoxString())
+   *    - format=json 
+   *    - q=query (the query string)
+   * @param {Leaflet.Map} map the Leaflet map
+   * @param {string} query The nominatim query. Example "Antwerpen", "Brugge", etc.
+   * 
+   * If the result of this function isn't satisfying, try to make a more specific query
+   * e.g. "Antwerpen Belgium", or simply set the coordinates manually.
+   */
+function focusOn(map, query) {
+  if(typeof query !== 'string') {
+    console.error("Query is not a string");
+    return;
+  }
+  // Prepare the request
+  let xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState != 4) return;
+    if  (this.status == 200)
+      handleQueryResult(this.responseText);
+    else
+      console.error("focusOn - Nominatim query failed with code %o: %o", this.status, this.responseText);
+  };
+  // Prepare the query
+  let queryURL = `https://nominatim.openstreetmap.org/search?q=${query}&format=json&viewbox=${map.getBounds().toBBoxString()}&limit=1`;
+  // Send the request
+  xhttp.open("GET", encodeURI(queryURL));
+  xhttp.send();
+  /**
+   * Handles the result of a successful nominatim query.
+   * @param {string} resultString the result string of the query. Must be JSON
+   */
+  function handleQueryResult(resultString) {
+    let result;
+    try {
+      result = JSON.parse(resultString);
+    }
+    catch(err) {
+      console.error("focusOn - an error occured while parsing the Nominatim query result: %o", err);
+      return;
+    }
+    // Fetch the first result. If there's no result, don't do anything.
+    result = result[0];
+    if(result == undefined) {
+      console.log("focusOn - No result found for query '%o'", query);
+      return;
+    }
+    // Center the map on the lat/lon
+    map.setView(L.latLng(result.lat, result.lon));
+  }
 }
 
 /**
  * Queries the Overpass API and adds the returned points on the map.
  * This is asynchronous, and the points will be added once the query is completed.
  * @param {Leaflet.Map} map the map
- * @param {String} query the query
- * @param {Configuration} configuration the configuration object. can be null.
+ * @param {string} query the query
+ * @param {Configuration} conf the configuration object. can be null.
  */
-function queryAndShowFeatures(map, query, configuration) {
+function queryAndShowFeatures(map, query, conf) {
   // Create a configuration object if we don't have one
   if (configuration == undefined)
     configuration = new Configuration();
@@ -250,7 +256,7 @@ function queryAndShowFeatures(map, query, configuration) {
 
   /**
   * Handles the result of a successful query to the Overpass API, adding the features as markers on the map.
-  * @param {String} rawJSON the raw JSON String
+  * @param {string} rawJSON the raw JSON string
   * @param {Configuration} configuration the configuration object. can be null.
   */
   function handleQueryResult(rawJSON, configuration) {
@@ -261,12 +267,12 @@ function queryAndShowFeatures(map, query, configuration) {
     }
     catch (err) {
       console.error("JSON Parsing Error!");
-      console.log("String that we tried to parse: %o", rawJSON);
+      console.log("string that we tried to parse: %o", rawJSON);
       console.log("JSON.parse() Result: %o", json);
       return;
     }
 
-    let maxElements = configuration.maxElements;
+    let maxElements = conf.maxElements;
     // Once that's done, gather the list of features. They're located in the "elements" part of the
     // JSON.
     if (json.elements != undefined) {
